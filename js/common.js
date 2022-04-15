f1a9804f267ad993bafd3d8b7ed00f54edd12d6a
jinsil <developer_corn@daum.net>
2022-04-16 02:04:15 +0900

    update main design

--- js/common.js
+++ js/common.js
@@ -2,4 +2,10 @@ $(function () {
     $('#gnb__btn--burger').on('click', function () {
         $('#gnb').toggleClass('on');
     });
+
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
 });
